e01c600614f42c64c3fcb542f37ebaaacd1147f4
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2018-02-15 15:54:43 -0600

    refactor: modify get time function

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function render(prod){
 
       if (time.length > 1) {
         time = time.slice (1);
-        time[5] = +time[0] < 12 ? ' AM' : ' PM';
+        time[5] = +time[0] < 12 ? ' AM':' PM';
         time[0] = +time[0] % 12 || 12;
       }
       return time.join ('');
