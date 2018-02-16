3690946da6c085efb07260d6c7a218fe570593c4
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2018-02-15 23:47:38 -0600

    refactor: modify function to get 'AM,PM' format
    to moments.js function

--- src/index.js
+++ src/index.js
@@ -14,21 +14,102 @@ global.jQuery = $;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-  // Function to manage slider
-  var slideIndex = 0;
-  showSlides(slideIndex);
 
-  function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1} 
-      slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 7000);
+
+// Component to show json data
+const { Component } = React;
+const { render } = ReactDOM;
+
+class jsonData extends Component {
+  // Constructor
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    };
+  };
+  // Get data from json file
+  componentDidMount() {
+    fetch("data.json")
+    .then( (response) => {
+      return response.json() })   
+    .then( (json) => {
+      this.setState({data: json});
+    });
+  };
+  // Render heach json object to container
+  render() {
+    var canvases = this.state.data.map( (data, index) => {
+      var m = moment.unix((parseInt(data.startDate)));
+      var s = m.format("M/D/YYYY");
+      var newhour = m.format("H:mm A");
+      var end = moment.unix((parseInt(data.endDate)));
+      var send = end.format("M/D/YYYY");
+      var lasthour = end.format("h:mm A");
+      return (
+        <div className="card card--container">
+        <div className="row">
+        <div className="col-6 d-flex flex-column justify-content-center align-items-center card__ticket">
+        <div className="">{data.ticket}</div>
+        </div>
+        <div className="col-6 d-flex flex-column justify-content-center align-items-center card__title">
+        <h1 className="">{data.full_name}</h1>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-6 d-flex flex-column justify-content-center align-items-center firstdate">
+        <div className="card__hour">{newhour}</div>
+        <div className="card__date">{s}</div>
+        </div>
+        <div className="col-6 d-flex flex-column justify-content-center align-items-center firstaddress">
+        <h3 className="card__start">{data.startPlaceName}</h3>
+        <p className="card__startaddress">{data.startPlaceAddress}</p>
+        </div>
+        </div>
+        <div className="row">
+        <div className="col-6 d-flex flex-column justify-content-center align-items-center lastdate">
+        <div className="card__endhour">{lasthour}</div>
+        <div className="card__enddate">{send}</div>
+        </div>
+        <div className="col-6 d-flex flex-column justify-content-center align-items-center lastaddress">
+        <h5 className="card__endaddressName">{data.endPlaceName}</h5>
+        <p className="card__endaddressPlace">{data.endPlaceAddress}</p>
+        </div>
+        </div>
+        </div>
+        );
+    });
+    // Create div where html will be render
+    return(
+      React.createElement('div', null, ...canvases)
+      );
+  };
+
+};
+
+// Parse data to DOM
+render(
+  React.createElement(jsonData, null),
+  document.getElementById('messagesContainer')
+  );
+
+
+// Jquery necesary to run slider plugin
+// Function to manage slider
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+  setTimeout(showSlides, 7000);
+}
 
 // Function to manage active class for menu items
 $("#mainNav li a").on("click", function(e) {
@@ -69,148 +150,8 @@ $('a[href*="#"]')
       });
     }
   });
-
-// Smooth scroll on 'what' section button
-$("#btndown").click(function() {
-  $('html,body').animate({
-    scrollTop: $("#root").offset().top},
-    2000);
-});
-
-
-const { Component } = React;
-const { render } = ReactDOM;
-
-class jsonData extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-      data: []
-    };
-  };
-
-  componentDidMount() {
-    fetch("data.json")
-    .then( (response) => {
-      return response.json() })   
-    .then( (json) => {
-      this.setState({data: json});
-    });
-  };
-
-  componentDidUpdate() {
-    function animation(json) {
-      return json.map( (data) => {
-        var m = moment.unix((parseInt(data.startDate)));
-        var s = m.format("M/D/YYYY");
-        var sr = m.format("H:mm");
-        var newhour = tConvert (sr);
-        function tConvert (time) {
-          time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-          if (time.length > 1) {
-            time = time.slice (1);
-            time[5] = +time[0] < 12 ? ' AM':' PM';
-            time[0] = +time[0] % 12 || 12;
-          }
-          return time.join ('');
-        }
-        var end = moment.unix((parseInt(data.endDate)));
-        var send = end.format("M/D/YYYY");
-        var srend = end.format("H:mm");
-        var lasthour = tConvert (srend);
-        function tConvert (time) {
-          time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-          if (time.length > 1) {
-            time = time.slice (1);
-            time[5] = +time[0] < 12 ? ' AM' : ' PM';
-            time[0] = +time[0] % 12 || 12;
-          }
-          return time.join ('');
-        }
-        return(`
-          `);
-      });
-    };
-        // this.setState({animation: animation(this.state.data)}); //causes infinite loop
-        // this.animation = animation(this.state.data);
-      };
-
-      render() {
-        var canvases = this.state.data.map( (data, index) => {
-          var m = moment.unix((parseInt(data.startDate)));
-          var s = m.format("M/D/YYYY");
-          var sr = m.format("H:mm");
-          var newhour = tConvert (sr);
-          function tConvert (time) {
-            time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-            if (time.length > 1) {
-              time = time.slice (1);
-              time[5] = +time[0] < 12 ? ' AM':' PM';
-              time[0] = +time[0] % 12 || 12;
-            }
-            return time.join ('');
-          }
-          var end = moment.unix((parseInt(data.endDate)));
-          var send = end.format("M/D/YYYY");
-          var srend = end.format("H:mm");
-          var lasthour = tConvert (srend);
-          function tConvert (time) {
-            time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-            if (time.length > 1) {
-              time = time.slice (1);
-              time[5] = +time[0] < 12 ? ' AM' : ' PM';
-              time[0] = +time[0] % 12 || 12;
-            }
-            return time.join ('');
-          }
-          console.log(data);
-          return (
-            <div class="card card--container">
-            <div class="row">
-            <div class="col-6 d-flex flex-column justify-content-center align-items-center card__ticket">
-            <div class="">${data.ticket}</div>
-            </div>
-            <div class="col-6 d-flex flex-column justify-content-center align-items-center card__title">
-            <h1 class="">${data.full_name}</h1>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col-6 d-flex flex-column justify-content-center align-items-center firstdate">
-            <div class="card__hour">${newhour}</div>
-            <div class="card__date">${s}</div>
-            </div>
-            <div class="col-6 d-flex flex-column justify-content-center align-items-center firstaddress">
-            <h3 class="card__start">${data.startPlaceName}</h3>
-            <p class="card__startaddress">${data.startPlaceAddress}</p>
-            </div>
-            </div>
-            <div class="row">
-            <div class="col-6 d-flex flex-column justify-content-center align-items-center lastdate">
-            <div class="card__endhour">${lasthour}</div>
-            <div class="card__enddate">${send}</div>
-            </div>
-            <div class="col-6 d-flex flex-column justify-content-center align-items-center lastaddress">
-            <h5 class="card__endaddressName">${data.endPlaceName}</h5>
-            <p class="card__endaddressPlace">${data.endPlaceAddress}</p>
-            </div>
-            </div>
-            </div>
-            );
-        });
-        return(
-          React.createElement('div', null, ...canvases)
-          );
-      };
-
-    };
-
-    
-    render(
-      React.createElement(jsonData, null),
-      document.getElementById('messages2')
-      );
+// $("#btndown").click(function() {
+//   $('html,body').animate({
+//     scrollTop: $("#root").offset().top},
+//     2000);
+// });
